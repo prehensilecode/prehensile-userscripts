b34ac97ba15a330942ee65c1f9cd7246af036339
David Chin <david.w.h.chin@gmail.com>
2013-03-25 19:10:19 -0300

    Update faceinvert.user.js
    Change URL trigger to https://www.facebook.com/8
    Change CSS

--- faceinvert.user.js
+++ faceinvert.user.js
@@ -2,13 +2,13 @@
 // @name           FaceInvert
 // @namespace      net.prehensileeye.faceinvert
 // @description    Flips FB upside-down
-// @include        http://www.facebook.com/*
+// @include        https://www.facebook.com/*
 // @author         David Chin <david@prehensileEye.net>
 // @version        1.0
 // @license        Public Domain
 // ==/UserScript==
 // Flips Facebook upside down. That's it,
 (function() {
-    GM_addStyle('body {-webkit-transform:rotate(180deg); -moz-transform:rotate(180deg);}');
+    GM_addStyle('body {transform:rotate(180deg);}');
 })();
 
