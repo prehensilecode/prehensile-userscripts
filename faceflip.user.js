a7b60c1eb352bbefbbf5ca14b41f7b404beb24a2
David Chin <david.w.h.chin@gmail.com>
2013-03-25 19:23:12 -0300

    Update faceflip.user.js
    Updated URL to https://www.facebook.com/*
    Updated CSS

--- faceflip.user.js
+++ faceflip.user.js
@@ -2,13 +2,13 @@
 // @name           FaceFlip
 // @namespace      net.prehensileeye.faceflip
 // @description    Flips FB images upside-down
-// @include        http://www.facebook.com/*
+// @include        https://www.facebook.com/*
 // @author         David Chin <david@prehensileEye.net>
 // @version        1.0
 // @license        Public Domain
 // ==/UserScript==
 // Flips all images in Facebook upside down. That's it,
 (function() {
-    GM_addStyle('img {-webkit-transform:rotate(180deg); -moz-transform:rotate(180deg);}');
+    GM_addStyle('img {transform:rotate(180deg); -webkit-transform:rotate(180deg); -moz-transform:rotate(180deg); -ms-transform:rotate(180deg); -o-transform:rotate(180deg);}');
 })();
 
